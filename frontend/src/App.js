4a15c7e5eaf44af15f1bb00407ae13e924a3adf3
shubhamrg25 <shubhamgungane25@gmail.com>
2025-12-17 17:27:01 +0530

    Initial frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Shop SaaS Billing Software</h1>
+      <p>Cloud based billing, stock & payments</p>
+    </div>
+  );
+}
+
+export default App;
